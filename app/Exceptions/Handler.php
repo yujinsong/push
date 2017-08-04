92f4a828b122495324417b618ca52a051fe7127d
yujinsong <yujinsong>
2017-08-04 09:11:12 +0800

    add nginx docker config

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,7 +47,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-       // return parent::render($request, $e);
+       return parent::render($request, $e);
         $err = [
             'request' => Request::all(),
             'response' => array(),
